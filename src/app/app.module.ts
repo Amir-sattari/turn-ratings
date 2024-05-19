bd434174385694fa1aaa967d4df5f25e79b30011
Amir Hossein <am.h.sattari@gmail.com>
2024-05-19 11:46:46 +0330

    salam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { ErrorFieldComponent } from './components/error-field/error-field.component';
-import { ValidationPipe } from './shared/pipes/validation.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    ErrorFieldComponent,
-    ValidationPipe
   ],
   imports: [
     BrowserModule,
