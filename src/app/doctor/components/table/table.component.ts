37e978d65cd2f2efa2e03fb8c44e955fa9863b63
Amir Hossein <am.h.sattari@gmail.com>
2024-07-27 11:13:40 +0330

    hello

--- src/app/doctor/components/table/table.component.ts
+++ src/app/doctor/components/table/table.component.ts
@@ -6,23 +6,37 @@ import { Component, Input } from '@angular/core';
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  @Input() showTurnButtons: boolean = false;
+  rowStatus!: 'accepted' | 'rejected';
 
-  // @Input() titles: any = ['ردیف','نام و نام خانوادگی','کد ملی','شماره همراه','تاریخ و ساعت مراجعه',]
-  // @Input() values: any = ['امیر حسین ستاری','4271637858','09214904528','هر موقع دلم خواست میام',]
+  count = [
+    { id: 1, name: 'امیر حسین', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 2, name: 'رادمان', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 3, name: 'یاسین', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 4, name: 'احمدرضا', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 5, name: 'محمد', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 6, name: 'علی', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 7, name: 'امیر', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 8, name: 'پویا', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 9, name: 'سعید', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+    { id: 10, name: 'اکبر', date: '12 بهمن 1403', time: '18:00', isAccepted: false, isRejected: false },
+  ];
 
-
-  // var a = [
-  //   {
-  //     id: 1,
-  //     name: "amir hossein"
-  //   }
-  // ]
-
-  public isTodayTurns(): boolean
-  {
-    if(window.location.href.includes('today-turns'))
-      return true;
-
-    return false;
+  public accepted(row: any, selectedRowId: number): void {
+    
+    let rowIndex = this.count.findIndex(x => x.id == selectedRowId);
+    this.count.splice(rowIndex, 1);
+    this.count.push(row);
+    row.isAccepted = true;
   }
+
+  public rejected(row: any, selectedRowId: number): void {
+
+    let rowIndex = this.count.findIndex(x => x.id == selectedRowId);
+    this.count.splice(rowIndex, 1);
+    this.count.push(row);
+    row.isRejected = true;
+  }
+
+
 }
